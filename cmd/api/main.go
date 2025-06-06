adcfed318ea04780f7624289fa666a0ff846bb66
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-06-06 18:36:50 +0200

    main: init db, db flags, get env for dns

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -7,23 +7,43 @@ import (
 	"net/http"
 	"os"
 	"time"
+
+	"github.com/Torkel-Aannestad/coop-backend/internal/database"
+	"github.com/joho/godotenv"
 )
 
 type Config struct {
 	port int
 	env  string
+	db   struct {
+		dsn          string
+		maxOpenConns int
+		maxIdleConns int
+		maxIdleTime  time.Duration
+	}
 }
 
 type application struct {
 	config Config
 	logger *slog.Logger
+	models *database.Models
 }
 
 func main() {
 	var cfg Config
 
+	godotenv.Load()
+	dsn := os.Getenv("COOP_DSN_DEV")
+
 	flag.IntVar(&cfg.port, "port", 4000, "API server port")
 	flag.StringVar(&cfg.env, "env", "development", "Environment (development|staging|production)")
+
+	//db
+	flag.StringVar(&cfg.db.dsn, "db-dsn", dsn, "dsn for PG instance")
+	flag.IntVar(&cfg.db.maxOpenConns, "db-max-open-conns", 25, "PostgreSQL max open connections")
+	flag.IntVar(&cfg.db.maxIdleConns, "db-max-idle-conns", 25, "PostgreSQL max idle connections")
+	flag.DurationVar(&cfg.db.maxIdleTime, "db-max-idle-time", 15*time.Minute, "PostgreSQL max connection idle time")
+
 	flag.Parse()
 
 	loggerOptions := &slog.HandlerOptions{
@@ -31,9 +51,12 @@ func main() {
 	}
 	logger := slog.New(slog.NewJSONHandler(os.Stdout, loggerOptions))
 
+	db, err := database.OpenDB(cfg.db.dsn, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
+
 	app := &application{
 		config: cfg,
 		logger: logger,
+		models: database.NewModels(db),
 	}
 	srv := &http.Server{
 		Addr:         fmt.Sprintf(":%d", app.config.port),
@@ -45,7 +68,7 @@ func main() {
 	}
 
 	logger.Info("starting server", "addr", srv.Addr, "env", app.config.env)
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	logger.Error(err.Error())
 	os.Exit(1)
 
--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/Torkel-Aannestad/coop-backend
 go 1.22.3
 
 require github.com/go-chi/chi/v5 v5.2.1
+
+require github.com/joho/godotenv v1.5.1
