a9c13e97c652e3cbdf1600440f1d93f9df1fb590
Torkel Aannestad <torkel.aannestad@outlook.com>
2025-06-09 14:01:40 +0200

    database migration

---
+++ sql/migrations/0001_messages_create.sql
@@ -0,0 +1,14 @@
+-- +goose Up
+CREATE TABLE IF NOT EXISTS messages (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,  
+    created_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    modified_at timestamp(0) with time zone NOT NULL DEFAULT NOW(),
+    external_id text NOT NULL UNIQUE,
+    author text NOT NULL,
+    title text NOT NULL,
+    body text NOT NULL,
+    platform text NOT NULL
+);
+
+-- +goose Down
+DROP TABLE IF EXISTS messages;
